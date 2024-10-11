e59dc21ef8784639b1e8d358c0171a736e246587
Joshua Batista <joshuabastista@live.com>
2024-10-11 11:17:53 -0300

    add

---
+++ src/modulos/publico/pages/publico-controle.php
@@ -0,0 +1,32 @@
+<?php
+
+ $title = "Monetra | Controle";
+
+ require '../../../includes/head.php'; 
+ require "../../../../app/functions.php";
+
+?>
+
+<html lang="pt-br">
+
+<?php require '../../../includes/header.php'; ?>
+
+<body>
+    <div class="content rounded-lg">
+    <form class="">
+        <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="email" id="email" class="bg-gray-50 border w-28 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        </div>
+    </form>
+
+
+
+    </div>
+
+
+
+    <?php require"../../../includes/footer.php"?>
+</body>
+
+</html>
