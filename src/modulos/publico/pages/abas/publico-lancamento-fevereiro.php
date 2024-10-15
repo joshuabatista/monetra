c84b90d3346bf636843aa87c4d3feb28a6a7b93b
Joshua Batista <joshuabastista@live.com>
2024-10-15 14:07:09 -0300

    add tabs lanctos

---
+++ src/modulos/publico/pages/abas/publico-lancamento-fevereiro.php
@@ -0,0 +1,4 @@
+<div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="fevereiro" role="tabpanel"
+    aria-labelledby="fevereiro-tab">
+    <p class="text-sm text-gray-500 dark:text-gray-400">fevereiro</p>
+</div>
