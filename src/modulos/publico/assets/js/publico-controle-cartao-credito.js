ff669250d8775f493466ddc7ad351916e3b02dff
Joshua Batista <joshuabatista@live.com>
2024-10-20 20:35:41 -0300

    add func de compras parceladas no cartão de credito

--- src/modulos/publico/assets/js/publico-controle-cartao-credito.js
+++ src/modulos/publico/assets/js/publico-controle-cartao-credito.js
@@ -32,12 +32,11 @@ const addMovimentationCard = () => {
         let planoContas = $('#selectPlanoContasCartao').val();
         let beneficiario = $('.beneficiarioCartao').val();
         let tipo = $('#tipoCartao').val();
-        let valor = $('#valorCartao').val();
+        let valor = parseFloat($('#valorCartao').val());    
         let cartao = $('#cartao').val();
-        let quantidade = $('#quantidade').val();
+        let quantidade = parseInt($('#quantidade').val())
         let dataFormatada = formatarData(data)
 
-        // Valida se os campos obrigatórios estão preenchidos
         if(!dataFormatada || !categoria || !planoContas || !tipo || !valor) {
             Swal.fire({
                 position: 'top-end',
@@ -77,23 +76,31 @@ const addMovimentationCard = () => {
             credito = `<span class="credito">R$ ${parseFloat(valor).toFixed(2)}</span>`;
         }
 
+        let valorParcela = (valor / quantidade).toFixed(2)
 
+        const adicionarMeses = (data, meses) => {
+            let novaData = new Date(data);
+            novaData.setMonth(novaData.getMonth() + meses);
+            return novaData.toLocaleDateString('pt-BR'); 
+        }
 
-        // Adiciona uma nova linha na tabela com os valores capturados
-        $('#tabelaCartoes tbody').append(`
-            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b">
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${dataFormatada}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${categoria.charAt(0).toUpperCase() + categoria.slice(1)}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${planoContas}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${cartao}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${quantidade}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${beneficiario}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${tipoTexto}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${debito}</td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${credito}</td>
-
-            </tr>
-        `);
+        for (let i = 1; i <= quantidade; i++) {
+            let dataParcela = adicionarMeses(data, i - 1); 
+    
+            $('#tabelaCartoes tbody').append(`
+                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b">
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${dataParcela}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${categoria.charAt(0).toUpperCase() + categoria.slice(1)}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${planoContas}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${cartao}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${i} de ${quantidade}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${beneficiario}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${tipoTexto}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${debito ? `-R$ ${valorParcela}` : ''}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">${credito ? `R$ ${valorParcela}` : ''}</td>
+                </tr>
+            `);
+        }
 
         // Limpa os campos após a inserção
         $('#dataCartao').val('');
