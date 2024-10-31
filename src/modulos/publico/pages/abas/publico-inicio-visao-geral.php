fae6fd1db973f716fa2197862cf8bd26ba9d65ae
Joshua Batista <joshuabastista@live.com>
2024-10-31 18:12:11 -0300

    add files for tabs

---
+++ src/modulos/publico/pages/abas/publico-inicio-visao-geral.php
@@ -0,0 +1,7 @@
+<div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="visao-geral" role="tabpanel"
+                    aria-labelledby="visao-tab">
+                    <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
+                            class="font-medium text-gray-800 dark:text-white">visao-geral tab's associated content</strong>.
+                        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript
+                        swaps classes to control the content visibility and styling.</p>
+                </div>
--- src/modulos/publico/pages/publico-inicio.php
+++ src/modulos/publico/pages/publico-inicio.php
@@ -18,62 +18,42 @@
         <div class="content rounded-r-xl">
 
             <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-                <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab"
-                    data-tabs-toggle="#default-tab-content" role="tablist">
-                    <li class="me-2" role="presentation">
-                        <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-tab"
-                            data-tabs-target="#profile" type="button" role="tab" aria-controls="profile"
+                <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="controle"
+                    data-tabs-toggle="#controle-content" role="tablist">
+
+                    <li class="me-2" role="presentation"> <button class="inline-block p-4 border-b-2 rounded-t-lg" id="visao-tab"
+                            data-tabs-target="#visao-geral" type="button" role="tab" aria-controls="visao-geral"
                             aria-selected="false">Visão Geral</button>
                     </li>
                     <li class="me-2" role="presentation">
-                        <button
-                            class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                        <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
                             id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab"
                             aria-controls="dashboard" aria-selected="false">Dashboard</button>
                     </li>
                     <li class="me-2" role="presentation">
                         <button
                             class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                            id="settings-tab" data-tabs-target="#settings" type="button" role="tab"
-                            aria-controls="settings" aria-selected="false">Controle Minucioso</button>
+                            id="minucioso-tab" data-tabs-target="#minucioso" type="button" role="tab"
+                            aria-controls="minucioso" aria-selected="false">Controle Minucioso</button>
                     </li>
                     <li role="presentation">
                         <button
                             class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                            id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab"
-                            aria-controls="contacts" aria-selected="false">Cartão de Crédito</button>
+                            id="cartao-tab" data-tabs-target="#cartao" type="button" role="tab"
+                            aria-controls="cartao" aria-selected="false">Cartão de Crédito</button>
                     </li>
                 </ul>
             </div>
-            <div id="default-tab-content">
-                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel"
-                    aria-labelledby="profile-tab">
-                    <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-                            class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>.
-                        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript
-                        swaps classes to control the content visibility and styling.</p>
-                </div>
-                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel"
-                    aria-labelledby="dashboard-tab">
-                    <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-                            class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated
-                            content</strong>. Clicking another tab will toggle the visibility of this one for the next.
-                        The tab JavaScript swaps classes to control the content visibility and styling.</p>
-                </div>
-                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="settings" role="tabpanel"
-                    aria-labelledby="settings-tab">
-                    <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-                            class="font-medium text-gray-800 dark:text-white">Settings tab's associated
-                            content</strong>. Clicking another tab will toggle the visibility of this one for the next.
-                        The tab JavaScript swaps classes to control the content visibility and styling.</p>
-                </div>
-                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="contacts" role="tabpanel"
-                    aria-labelledby="contacts-tab">
-                    <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-                            class="font-medium text-gray-800 dark:text-white">Contacts tab's associated
-                            content</strong>. Clicking another tab will toggle the visibility of this one for the next.
-                        The tab JavaScript swaps classes to control the content visibility and styling.</p>
-                </div>
+            <div id="controle-content">
+
+                    <?php include "../pages/abas/publico-inicio-visao-geral.php" ?>
+                    
+                    <?php include "../pages/abas/publico-inicio-dashboard.php" ?>
+
+                    <?php include "../pages/abas/publico-inicio-minucioso.php" ?>
+
+                    <?php include "../pages/abas/publico-inicio-cartao.php" ?>
+
             </div>
 
 
