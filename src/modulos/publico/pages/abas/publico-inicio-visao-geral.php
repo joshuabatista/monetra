376859a0478aa7ae6c3049b5ca87093edf297cd5
Joshua Batista <joshuabastista@live.com>
2024-11-01 12:44:50 -0300

    add front de saldos na tela inicial

--- src/modulos/publico/pages/abas/publico-inicio-visao-geral.php
+++ src/modulos/publico/pages/abas/publico-inicio-visao-geral.php
@@ -1,7 +1,52 @@
-<div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="visao-geral" role="tabpanel"
-                    aria-labelledby="visao-tab">
-                    <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-                            class="font-medium text-gray-800 dark:text-white">visao-geral tab's associated content</strong>.
-                        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript
-                        swaps classes to control the content visibility and styling.</p>
+<div class="hidden p-4 rounded-lg" id="visao-geral" role="tabpanel" aria-labelledby="visao-tab">
+    <div class="grid grid-cols-2">
+        <div>
+            <div class="movimentatios">
+                <div>
+                    <h1 class="mb-2 text-3xl">Movimentações do periodo</h1>
                 </div>
+            </div>
+            <div class="filters-period inline-flex flex-row bg-gray-100 p-5 rounded-t-xl">
+                <div class="col-data-inicio">
+                    <label for="data-inicio" class="label">Data inicio</label>
+                    <input type="date" id="data-inicio" name="data-inicio"
+                        class=" px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
+                </div>
+                <div class="col-data-termino">
+                    <label for="data-termino" class="label">Data termino</label>
+                    <input type="date" id="data-termino" name="data-termino"
+                        class=" px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
+                </div>
+    
+            </div>
+            <br>
+            <div class=" bg-gray-100 inline-flex flex-row gap-4 p-5 rounded-b-xl rounded-tr-xl shadow-xl">
+                <div class="">
+                    <label for="" class="text-2xl">Saldo Inicial</label>
+                    <h1 class="text-xl"><small><i class="fa-solid fa-money-bill-1 bg-slate-300 p-2 rounded-lg"></i></small> R$ 3.000,00</h1>
+                </div>
+                <div class="">
+                    <label for="" class="text-2xl">Entradas</label>
+                    <h1 class="text-xl"><small><i class="fa-solid fa-arrow-trend-up bg-green-500 p-2 rounded-lg"></i></small> R$ 3.000,00</h1>
+                </div>
+                <div class="">
+                    <label for="" class="text-2xl">Saidas</label>
+                    <h1 class="text-xl"><small><i class="fa-solid fa-arrow-trend-down bg-red-500 p-2 rounded-lg"></i></small> R$ 3.000,00</h1>
+                </div>
+                <div class="">
+                    <label for="" class="text-2xl">Saldo Final</label>
+                    <h1 class="text-xl"><small><i class="fa-solid fa-money-bill-transfer bg-slate-300 p-2 rounded-lg"></i></small> R$ 3.000,00</h1>
+                </div>
+    
+            </div>
+        </div>
+
+        <div class="">
+            <div>
+                <h1 class="mb-2 text-3xl">Movimentações diarias</h1>
+            </div>
+
+        </div>
+    </div>
+
+</div>
